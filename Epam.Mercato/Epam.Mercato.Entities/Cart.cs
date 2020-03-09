d889326d2e5d476b80a3cc7a9a851a2eab56e0a2
ezbrz <54773657+ezbrz@users.noreply.github.com>
2020-03-10 00:41:38 +0400

    Final task

---
+++ Epam.Mercato/Epam.Mercato.Entities/Cart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Epam.Mercato.Entities
+{
+    public static class Cart
+    {
+        public static Dictionary<Product, int> CartForUser = new Dictionary<Product, int>();
+        public static string user;
+        static Cart()
+        {
+        }
+        public static void AddItem(Product prod, int count)
+        {
+            CartForUser.Add(prod, count);
+        }
+        public static void RemoveItem(Product prod)
+        {
+            CartForUser.Remove(prod);
+        }
+        public static void ClearCart()
+        {
+            CartForUser.Clear();
+        }
+        public static decimal Compute()
+        {
+            return CartForUser.Sum(x => x.Key.Price * x.Value);
+        }
+        public static void Purchase()
+        {
+
+        }
+    }
+}
